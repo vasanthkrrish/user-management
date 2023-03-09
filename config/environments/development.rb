39e0667beaf7b3b11c99145e48a90670e55b3749
Vasantha-idexcel <vasantha.r@idexcel.com>
2023-03-09 05:43:30 +0530

    Added devise model user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,17 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    user_name: 'vasanth765476@gmail.com',
+    password: 'nzowkrbrtyucgcto',
+    domain: 'gmail.com',
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain
+  }
 end
