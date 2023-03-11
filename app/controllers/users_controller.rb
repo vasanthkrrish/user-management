4841f7aeb680ec933cb289261ceec316a52d5ab2
Vasantha-idexcel <vasantha.r@idexcel.com>
2023-03-11 12:25:17 +0530

    After user update redirect to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+        format.html { redirect_to user_path, notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
         flash[:error] = @user.errors.full_messages.uniq.join(', ')
